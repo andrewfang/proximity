db9e48dd1dd953684d7968b10fdbcf5b531824f7
Andrew Fang <andrewbrianfang@gmail.com>
2015-03-01 01:06:08 -0800

    New version of the website

--- web/js/script.js
+++ web/js/script.js
@@ -1,13 +1,26 @@
-var refresh = function() {
-  var h = window.innerHeight;
-  var element = document.getElementById("main");
-  element.style.height = h + "px";
-
-  //var title = document.getElementById("title");
-  //title.style.marginTop = (h - 200)/2 + "px";
+var refresh = function(divName) {
+  if (window.innerWidth > 990) {
+    var h = window.innerHeight;
+    var element = document.getElementById(divName);
+    element.style.height = Math.max(h - 71, 550) + "px";
+  } else {
+    var element = document.getElementById(divName);
+    element.style.height = "750px"
+  }
 };
-refresh();
+var resizeUpcoming = function() {
+  document.getElementById("upcomingBody").style.backgroundSize = window.innerHeight + 70 + "px"
+};
+var adjustSize = function() {
+  refresh("homeBody");
+  refresh("demoBody");
+  refresh("upcomingBody");
+  refresh("aboutBody");
+  refresh("contactBody");
+  resizeUpcoming();
+}
 
 window.onresize = function(event) {
-  refresh();
+  adjustSize();
 }
+adjustSize();
