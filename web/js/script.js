a816204100f18052763dd916def226361a7f9293
Andrew Fang <andrewbrianfang@gmail.com>
2015-03-01 20:03:47 -0800

    Launching mobile version

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+  adjustSize();
+  initScrollTo();
+  if (window.location.pathname != "/") {
+    doScrollTo(window.location.pathname.substring(1));
+  }
+});
 var refresh = function(divName) {
   if (window.innerWidth > 990) {
     var h = window.innerHeight;
@@ -5,12 +12,14 @@ var refresh = function(divName) {
     element.style.height = Math.max(h - 71, 550) + "px";
   } else {
     var element = document.getElementById(divName);
-    element.style.height = "750px"
+    element.style.height = "600px"
   }
-};
+}
+
 var resizeUpcoming = function() {
   document.getElementById("upcomingBody").style.backgroundSize = window.innerHeight + 70 + "px"
 };
+
 var adjustSize = function() {
   refresh("homeBody");
   refresh("demoBody");
@@ -23,4 +32,21 @@ var adjustSize = function() {
 window.onresize = function(event) {
   adjustSize();
 }
-adjustSize();
+
+var initScrollTo = function() {
+  var navbuttons = $(".navbutton");
+  for (idx in navbuttons) {
+    id = navbuttons[idx].id;
+    $("#" + id).click(function(event) {
+      event.preventDefault();
+      doScrollTo(this.id.substring(3));
+    });
+  }
+}
+
+var doScrollTo = function(destination) {
+  $('html, body').animate({
+      scrollTop: $("#" + destination).offset().top
+  }, 1000);
+  history.replaceState(null, null, destination);
+}
